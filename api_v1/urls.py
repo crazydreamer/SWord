1c3e66b2bb709b113a85145432615472c05bbb11
Stdio A <stdio_dai@nuaa.edu.cn>
2016-05-14 17:16:57 +0800

    API 文件名更改

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,14 +1,14 @@
 # coding: utf-8
 
 from django.conf.urls import include, url
-import views
+import words
 import memorize as memo
 
 app_name = 'api_v1'
 urlpatterns = [
-    url(r'^word/(?P<word_id>[0-9]+)/note/?$', views.note, name='note'),
-    url(r'^word/(?P<word_id>[0-9]+)/?$', views.word, name='word'),
-    url(r'^search/?$', views.search, name='search'),
+    url(r'^word/(?P<word_id>[0-9]+)/note/?$', words.note, name='note'),
+    url(r'^word/(?P<word_id>[0-9]+)/?$', words.word, name='word'),
+    url(r'^search/?$', words.search, name='search'),
     url(r'^memo_status/(?P<learning_id>[0-9]+)?/?$', memo.status, name='status'),
     url(r'^memo_finish/?$', memo.finish, name='finish'),
 ]
