58655073e3e25e1ee3de4fa6d9a19bcddedf7991
Stdio A <stdio_dai@nuaa.edu.cn>
2016-05-15 22:39:28 +0800

    修改单词选择测试的位置

--- memo/tests.py
+++ memo/tests.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
+from datetime import timedelta
 from django.test import TestCase, Client
 from django.utils import timezone
 from .models import Note, Word, Vocabulary, UserProfile
 from django.contrib.auth.models import User
-from api_v1.memorize import __select_word as select_word
 
 
 # Create your tests here.
@@ -35,45 +35,6 @@ class UserProfileTests(TestCase):
         self.assertEqual(self.up.daily_words_amount, 20)
         self.assertEqual(self.up.memorized_words.count(), 0)
         self.assertIsNone(self.up.current_vocabulary)
-
-
-class WordSelectingTests(TestCase):
-    def setUp(self):
-        self.voc = Vocabulary(name="voc")
-        self.voc.save()
-        self.user = User(username="user", password="password")
-        self.user.save()
-        self.up = UserProfile(user=self.user,
-                                current_vocabulary=self.voc,
-                                daily_words_amount=35)
-        self.up.save()
-        f = file("scripts/vocabulary.txt", "r")
-
-        for lno, line in enumerate(f):
-            word, exp = line.strip().split(" ", 1)
-
-            w = Word(content=word, description=exp)
-            w.save()
-            w.vocabulary = [self.voc]
-            w.save()
-            if lno >= 49:                                                       # 只添加 50 个单词
-                break
-
-        f.close()
-
-    def test_select_words(self):
-        u"""Testing the words selected randomly"""
-        words = select_word(self.user)
-        words_set = list(words)
-        self.assertEqual(len(words_set), self.up.daily_words_amount)
-
-        for w in words_set:                                                     # 将第一组单词添加到已掌握单词
-            self.up.memorized_words.add(w)
-
-        words = select_word(self.user)
-        words_set = list(words)
-        # 共 50 单词，已掌握 35 个，第二组只能选出 15 个
-        self.assertEqual(len(words_set), 15)
 
 
 class ProfileViewTests(TestCase):
@@ -136,8 +97,11 @@ class WordViewTests(TestCase):
         self.word.save()
         self.user = User.objects.create_user("user", "", "password")
         self.notes = []
+
+        start_time = timezone.now()
         for i in range(3):
-            n = Note(word=self.word, content="content%d"%i, user=self.user)
+            n = Note(word=self.word, content="content%d"%i, 
+                        user=self.user, time=start_time+timedelta(0, i, 0))     # 三次笔记间隔 1s
             self.notes.append(n)
             n.save()
 
